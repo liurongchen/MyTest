f0917da4d66ad8c4190b8bdb299e167a39a25cb0
liurongchen <liurongchen@aliyun.com>
2018-01-24 21:32:51 +0800

    first commit

--- hello-world/src/cn/ygsoft/rpa/test/Test.java
+++ hello-world/src/cn/ygsoft/rpa/test/Test.java
@@ -1,16 +1,10 @@
 package cn.ygsoft.rpa.test;
 
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Test {
     public static void main(String[] args){
-        System.out.println();
-
-        List<String> list = new ArrayList();
-
-        System.out.println();
+        System.out.println("hello-world!");
     }
 
 
