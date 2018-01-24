52bed06af63ced2bc8179845f87bf776e23c9c76
liurongchen <liurongchen@aliyun.com>
2018-01-24 21:26:23 +0800

    hello-world

---
+++ hello-world/src/cn/ygsoft/rpa/test/Test.java
@@ -0,0 +1,17 @@
+package cn.ygsoft.rpa.test;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args){
+        System.out.println();
+
+        List<String> list = new ArrayList();
+
+        System.out.println();
+    }
+
+
+}
